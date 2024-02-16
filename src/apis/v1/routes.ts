17d6b789bf5f9be756c12e397811e6c73f88be63
Takao Mizuno <waterhawk0032@gmail.com>
2024-02-17 09:51:19 +1000

    comment out endpoints

--- src/apis/v1/routes.ts
+++ src/apis/v1/routes.ts
@@ -15,37 +15,37 @@ export const registeroutes = (): Router => {
         res.send("Hello AUDJPYNot server!");
     });
 
-    router.get(`/rate`, async (req, res) => {
-        let result = {
-            baseCurrency: "",
-            quoteCurrency: "",
-            updateDate: "",
-            rate: 0,
-        };
+    // router.get(`/rate`, async (req, res) => {
+    //     let result = {
+    //         baseCurrency: "",
+    //         quoteCurrency: "",
+    //         updateDate: "",
+    //         rate: 0,
+    //     };
 
-        const baseCurrency = (req.query.base_currencty as string) ?? "AUD";
-        const quoteCurrency = (req.query.quote_currency as string) ?? "JPY";
-        try {
-            const rateInfo = await getRate(baseCurrency, quoteCurrency);
-            if (rateInfo !== undefined) {
-                result = {
-                    baseCurrency: baseCurrency,
-                    quoteCurrency: quoteCurrency,
-                    updateDate: rateInfo.updateDate,
-                    rate: rateInfo.rate,
-                };
-            }
+    //     const baseCurrency = (req.query.base_currencty as string) ?? "AUD";
+    //     const quoteCurrency = (req.query.quote_currency as string) ?? "JPY";
+    //     try {
+    //         const rateInfo = await getRate(baseCurrency, quoteCurrency);
+    //         if (rateInfo !== undefined) {
+    //             result = {
+    //                 baseCurrency: baseCurrency,
+    //                 quoteCurrency: quoteCurrency,
+    //                 updateDate: rateInfo.updateDate,
+    //                 rate: rateInfo.rate,
+    //             };
+    //         }
 
-            res.send(JSON.stringify(result));
-        } catch (error) {
-            res.status(500).send;
-        }
-    });
+    //         res.send(JSON.stringify(result));
+    //     } catch (error) {
+    //         res.status(500).send;
+    //     }
+    // });
 
-    router.get("/notification", (req, res) => {
-        sendNotifiction();
-        res.send("Notification sent!");
-    });
+    // router.get("/notification", (req, res) => {
+    //     sendNotifiction();
+    //     res.send("Notification sent!");
+    // });
 
     return router;
 };
