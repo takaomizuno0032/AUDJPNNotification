00f5a37a86edecdce744697acdd1ede8335891a2
Takao Mizuno <waterhawk0032@gmail.com>
2024-02-16 15:31:41 +1000

    fix entry point

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import * as bodyParser from "body-parser";
+import { loadDotEnv, getEnv } from "./services/env_service";
+loadDotEnv();
+import { registerRoutes } from "./routes";
+const HTTP_BODY_LIMIT = "500mb";
+
+const app = express();
+app.use(bodyParser.json({ limit: HTTP_BODY_LIMIT }));
+app.use(bodyParser.urlencoded({ limit: HTTP_BODY_LIMIT, extended: true }));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+registerRoutes(app);
+
 function main() {
- console.log("hello");
+    const port = getEnv("APP_PORT") || "3000";
+    app.listen(port, () => {
+        console.log("Port:", port);
+    });
 }
 
-main()
+main();
